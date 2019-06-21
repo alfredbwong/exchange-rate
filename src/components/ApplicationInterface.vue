0d61a7eae1b97d48ecec18a993ad6e6bf7634255
Alfred Wong <alfred.b.wong@gmail.com>
2019-06-21 00:56:57 -0400

    add template google chart

--- src/components/ApplicationInterface.vue
+++ src/components/ApplicationInterface.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="interface">
     <ControlPanel v-on:getRates="updateExchangeRates"/>
-    <div class="item2">Something</div>
+    <TimeSeriesGraph class="item2" v-bind:historicalData="historicalData" />
     <InfoPanel v-bind:info="info"/>
   </div>
 </template>
@@ -11,10 +11,12 @@ const axios = require('axios');
 
 import ControlPanel from './interface/ControlPanel.vue'
 import InfoPanel from './interface/InfoPanel.vue'
+import TimeSeriesGraph from './interface/view/TimeSeriesGraph.vue'
 export default {
   
    data : function (){
     return {
+      historicalData: null,
       info: null,
       currencyValueSelected: null
     }
@@ -22,7 +24,8 @@ export default {
   name: 'interface',
   components: {
     ControlPanel,
-    InfoPanel
+    InfoPanel,
+    TimeSeriesGraph
   },
    methods: {
 
@@ -31,14 +34,42 @@ export default {
       this.currencyValueSelected = currencyValueSelected;
       const vm = this;
       axios.all([
-        this.getCurrentRates(), 
+        this.getCurrentRates(),
+        this.getHistorialData()
       ])
-      .then(axios.spread(function(rateData){
-        vm.info = rateData.data.rates
+      .then(axios.spread(function(rateData, historicalData){
+        vm.info = rateData.data.rates,
+        vm.historicalData = historicalData.data.rates
       }))
     },
     getCurrentRates() {
       return axios.get('https://api.exchangeratesapi.io/latest?base=' + this.currencyValueSelected)
+    },
+    getHistorialData() {
+      var url = 'https://api.exchangeratesapi.io/history?start_at='+this.getPastMonthDate()+'&end_at='+this.getCurrentDate()+'&base=CAD&symbols=USD'
+      console.log(url)
+      return axios.get(url)
+    },
+    getCurrentDate(){
+      console.log("here")
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //As January is 0.
+      var yyyy = today.getFullYear();
+
+      if(dd<10) dd='0'+dd;
+      if(mm<10) mm='0'+mm;
+      return (yyyy+'-'+mm+'-'+dd);
+    },
+    getPastMonthDate(){
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth();
+      var yyyy = today.getFullYear();
+
+      if(dd<10) dd='0'+dd;
+      if(mm<10) mm='0'+mm;
+      return (yyyy+'-'+mm+'-'+dd);
     }
   }
 }
@@ -49,7 +80,8 @@ export default {
 #interface{
   border: 1px solid;
   display:grid;
-  grid-template-columns: 30% auto;
+  grid-template-columns: 25% auto;
+  grid-template-rows: 100px 700px;
   grid-gap: 20px;
 }
 .item2{
---
+++ src/components/interface/view/TimeSeriesGraph.vue
@@ -0,0 +1,42 @@
+<template>
+        <GChart
+            type="LineChart"
+            :data="chartData"
+            :options="chartOptions"
+        />
+</template>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+    export default {
+        components: {
+            GChart
+        },
+        data () {
+            return {
+                // Array will be automatically processed with visualization.arrayToDataTable function
+                
+                chartData: [
+                    ['Year', 'Sales', 'Expenses', 'Profit'],
+                    ['2014', 1000, 400, 200],
+                    ['2015', 1170, 460, 250],
+                    ['2016', 660, 1120, 300],
+                    ['2017', 1030, 540, 350]
+                ],
+                chartOptions: {
+                    chart: {
+                    title: 'Company Performance',
+                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                    }
+                }
+            }
+        },
+        props: ['historicalData']
+    }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
